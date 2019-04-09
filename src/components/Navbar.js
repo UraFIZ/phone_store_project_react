e2e5332e35087e4bd54048779a3aa60660bde007
UraFIZ <kratser88@gmail.com>
2019-04-09 20:49:39 +0300

    styling navbar is done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
 
 class Navbar extends Component {
   render() {
-    return <div>Hello from Navbar component</div>;
+    return (
+      <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my button
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
   }
 }
-
+const NavWrapper = styled.nav`
+  background-color: var(--mainBlue) !important;
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 export default Navbar;
